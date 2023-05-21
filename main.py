9bc82d4a3606c6827fc9fd4c8f4ee990814d9d84
Taky0nDon <126304313+Taky0nDon@users.noreply.github.com>
2023-05-21 08:22:10 -0400

    fixed formatting

--- main.py
+++ main.py
@@ -3,6 +3,5 @@ from quiz_brain import QuizBrain
 import ui
 
 
-
 quiz = QuizBrain(data.give_questions_to_user(category_id=None))
 quiz_ui = ui.QuizInterface(quiz)  # passing the QuizBrain instance to the quiz_ui
